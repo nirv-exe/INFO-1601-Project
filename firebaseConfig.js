791f99b83ad64652647873e440a3d43fcf0eb420
nirv-exe <niravpaltoo12@gmail.com>
2025-04-10 11:46:26 -0400

    firebase config

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQnclcqQXfnqZekRmc5kmY6ZgUlfj3zQI",
+  authDomain: "cramio.firebaseapp.com",
+  projectId: "cramio",
+  storageBucket: "cramio.firebasestorage.app",
+  messagingSenderId: "282798341002",
+  appId: "1:282798341002:web:6434b8fb9c1163dca0a673"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
